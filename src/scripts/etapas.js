a442f1a8e6b2121e7a066808d1cbc83132dc8c3b
Thiago Andrade <111758823+im4k1r4@users.noreply.github.com>
2023-01-23 20:43:30 -0300

    Add files via upload

---
+++ src/scripts/etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11111',
+                nome: 'Hera Venenosa',
+                partido: 'Vilãs Unidas',
+                fotos: [{url:'../images/11111.jpg', legenda: 'Vereadora', small: false}]
+            },
+            {
+                numero: '44444',
+                nome: 'Asa Noturna',
+                partido: 'Gotham Knights',
+                fotos: [{url: '../images/44444.jpg', legenda: 'Vereador'}]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '11',
+                nome: 'Arlequina',
+                partido: 'Vilãs Unidas',
+                vice: 'Mulher Gato',
+                fotos: [
+                    {url: '../images/11.jpg', legenda: 'Prefeita'},
+                    {url: '../images/11_2.jpg', legenda: 'Vice-Prefeita', small: true}
+                ]
+            },
+            {
+                numero: '44',
+                nome: 'Batwoman',
+                partido: 'Gotham Knights',
+                vice: 'Alfred',
+                fotos: [
+                    {url: '../images/44.jpg', legenda: 'Prefeita'},
+                    {url: '../images/44_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
